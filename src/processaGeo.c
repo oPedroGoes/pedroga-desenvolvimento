ea21b39485dba569cab8cfc9adef567e42ac78f7
oPedroGoes <pedrogoesassis@live.com>
2025-05-29 00:15:29 -0300

    Adiciona processaGeo para ser terminado.

---
+++ src/processaGeo.c
@@ -0,0 +1,236 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//#include "lancadores.h"
+//#include "linha.h"
+#include "circulo.h"
+#include "texto.h"
+#include "retangulo.h"
+//#include "arvorebin.h"
+//#include "formas.h"
+
+int ma(float x1, float x2, float y1, float y2){
+    if(x1<x2){
+        return 1;
+    } else if(x2<x1){
+        return 2;
+    } else{
+        if(y1<=y2){
+            return 3;
+        } else{
+            return 4;
+        }
+    }
+}
+
+/*AbdTree*/ void leitura_geo(FILE **ssvg1, FILE *arqgeo, int *instru, int * formcriadas, int *id, float *xcent, float *ycent) {
+    char* str = (char*)malloc(500 * sizeof(char));
+    char* tipo = (char*)malloc(3 * sizeof(char));
+    char* fam = NULL;
+    char* wei = NULL;
+    char* size = NULL;
+    int contadorforms=1;
+    //AbdTree a1;
+    //FORMASGEO f1;
+    while (fgets(str, 500, arqgeo)) {
+        strncpy(tipo, str, 3);
+        tipo[2] = '\0';
+        if (strcmp(tipo, "ts") == 0) {
+            free(fam);
+            free(wei);
+            free(size);
+            fam = (char*)malloc(15 * sizeof(char));
+            size = (char*)malloc(15 * sizeof(char));
+            wei = (char*)malloc(15 * sizeof(char));
+            sscanf(str + 2, "%s %s %s", fam, wei, size);
+        }
+
+        if (strcmp(tipo, "c ") == 0) {
+            int i;
+            float x, y, r;
+            char* cb = (char*)malloc(15 * sizeof(char));
+            char* cp = (char*)malloc(15 * sizeof(char));
+            sscanf(str + 2, "%d %f %f %f %s %s", &i, &x, &y, &r, cb, cp);
+            *id=i;
+            CIRCLE c = create_circle(i, x, y, r, cb, cp);
+            if(contadorforms==1){
+
+/////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                /*a1=createAbd(x, y, 0.0001);                
+                *xcent=x;
+                *ycent=y;
+                f1=setforma(c, 1);      // Talvez, esse não seja específico do edu.
+                insertAbd(a1, x, y, f1);*/
+/////////////////////////////////////////////////////////////////////////////////////////
+                contadorforms++;
+            }else{
+
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                f1=setforma(c, 1);      // Talvez, esse não seja específico do edu.
+                insertAbd(a1, x, y, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/
+            }
+            free(cb);
+            free(cp);
+            *formcriadas=*formcriadas+1;
+        }
+
+        if (strcmp(tipo, "r ") == 0) {
+            int i;
+            float x, y, h, w;
+            char* cb = (char*)malloc(15 * sizeof(char));
+            char* cp = (char*)malloc(15 * sizeof(char));
+            sscanf(str + 2, "%d %f %f %f %f %s %s", &i, &x, &y, &w, &h, cb, cp);
+            *id=i;
+            RECTANGLE r = cria_retangulo(i, x, y, w, h, cb, cp);
+            if(contadorforms==1){
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                *xcent=acharbaricentroRetX(x, w);
+                *ycent=acharbaricentroRetY(y, h);
+                a1=createAbd(acharbaricentroRetX(x, w), acharbaricentroRetY(y, h), 0.0001);
+                f1=setforma(r, 2);
+                insertAbd(a1, x, y, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/                
+                contadorforms++;
+            }else{
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                f1=setforma(r, 2);
+                insertAbd(a1, x, y, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/
+            }
+            free(cb);
+            free(cp);
+            *formcriadas=*formcriadas+1;
+        }
+
+        if (strcmp(tipo, "l ") == 0) {
+            int i;
+            float x1, y1, x2, y2;
+            char* c = (char*)malloc(15 * sizeof(char));
+            sscanf(str + 2, "%d %f %f %f %f %s", &i, &x1, &y1, &x2, &y2, c);
+            *id=i;
+            LINHA l = cria_linha(i, x1, y1, x2, y2, c);
+            int maior=ma(x1, x2, y1, y2);
+            if(maior==1 || maior ==3){
+                if(contadorforms==1){
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                *xcent=acharbaricentroLinX(x1, x2);
+                *ycent=acharbaricentroLinY(y1, y2);
+                a1=createAbd(acharbaricentroLinX(x1, x2), acharbaricentroLinY(y1, y2), 0.0001);
+                f1=setforma(l, 4);
+                insertAbd(a1, x1, y1, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/                    
+                    contadorforms++;
+                }else{
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                f1=setforma(l, 4);
+                insertAbd(a1, x1, y1, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/                    
+                }
+            } else{
+                if(contadorforms==1){
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                *xcent=acharbaricentroLinX(x1, x2);
+                *ycent=acharbaricentroLinY(y1, y2);
+                a1=createAbd(acharbaricentroLinX(x1, x2), acharbaricentroLinY(y1, y2), 0.0001);
+                f1=setforma(l, 4);
+                insertAbd(a1, x2, y2, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/                    
+                    contadorforms++;
+                }else{
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                f1=setforma(l, 4);
+                insertAbd(a1, x2, y2, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/                    
+                }
+            }
+            free(c);
+            *formcriadas=*formcriadas+1;
+        }
+
+        if (strcmp(tipo, "t ") == 0) {
+            int i;
+            float x, y;
+            char* cb = (char*)malloc(15 * sizeof(char));
+            char* cp = (char*)malloc(15 * sizeof(char));
+            char a;
+            char* txt = (char*)malloc(300 * sizeof(char));
+            sscanf(str + 2, "%d %f %f %s %s %c %[^\n]", &i, &x, &y, cb, cp, &a, txt);
+            *id=i;
+            TEXTO t = cria_texto(i, x, y, cb, cp, a, txt, fam, wei, size);
+            if(contadorforms==1){
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                *xcent=acharbaricentroLinX(x, get_eixoX2T(t));
+                *ycent=acharbaricentroLinY(y, y);
+                    a1=createAbd(acharbaricentroLinX(x, get_eixoX2T(t)), acharbaricentroLinY(y, y), 0.0001);
+                    f1=setforma(t, 3);
+                    insertAbd(a1, x, y, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/                
+                contadorforms++;
+            }else{
+/*////////////////////////////////////////////////////////////////////////////////////////
+                //COMANDOS ESPECIFICOS DO EDU
+                f1=setforma(t, 3);
+                insertAbd(a1, x, y, f1);
+////////////////////////////////////////////////////////////////////////////////////////*/                
+            }
+            free(cb);
+            free(cp);
+            free(txt);
+            *formcriadas=*formcriadas+1;
+        }
+        *instru=*instru+1;
+    }
+    percursoSimetrico(a1, &printasvg, ssvg1);
+    free(str);
+    free(tipo);
+    free(fam);
+    free(size);
+    free(wei);
+    return a1;
+}
+
+/*AbdTree*/ processa_geo(const char* pathgeo, const char* dirsaida, const char* nomegeo, int *instru, 
+                     int *formcriadas, int *id, float *xcent, float *ycent) {
+
+    FILE* arqgeo = fopen(pathgeo, "r");
+    printf("Diretório do arquivo geo: %s\n", pathgeo);
+    if (arqgeo==NULL) {
+        fprintf(stderr, "Erro na abertura do arquivo GEO: %s\n", pathgeo);
+        fclose(arqgeo);
+        exit(1);
+    }
+
+    char saidasvg1[512];
+        saidasvg1[0]='\0';
+        strcat(saidasvg1, dirsaida);
+        strcat(saidasvg1, nomegeo);
+        strcat(saidasvg1, ".svg");
+        printf("Diretório do arquivo svg1: %s\n", saidasvg1);
+    FILE* ssvg1 = fopen(saidasvg1, "w");
+    if (ssvg1==NULL) {
+        fprintf(stderr, "Erro na criação do arquivo SVG: %s\n", saidasvg1);
+        fclose(arqgeo);
+        fclose(ssvg1);
+        exit(1);
+    }
+
+    fprintf(ssvg1, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+    fprintf(ssvg1, "<svg width=\"2000\" height=\"2000\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">\n");
+
+    /*AbdTree*/ leitura_geo(&ssvg1, arqgeo, instru, formcriadas, id, xcent, ycent);
+
+    fprintf(ssvg1, "</svg>\n");
+    fclose(arqgeo);
+    fclose(ssvg1);
+    //return a1;
+}
---
+++ src/processaGeo.h
@@ -0,0 +1,24 @@
+#ifndef processageo_h
+#define processageo_h
+    /*
+    Esse cabeçalho header abre o arquivo .geo e insere em um árvore binária de busca todas as formas presentes no arquivo geo. Ele também retorna a estrutura da Arvore para ser utilizada em outras partes do codigo
+    */
+    typedef void* ARVORE;
+    
+    /**
+     * 
+     * @brief Função para ler o arquivo geo e processar o que está dentro dele, ela cria o arquivo svg 1 contendo as formas antes dos lançamentos
+     * 
+     * @param pathgeo String contendo o path para o arquivo geo para poder abrir ele
+     * @param dirsaid String contendo o path para os arquivos de saida
+     * @param nomegeo String contendo o nome do arquivo geo para estar presente no nome dos arquivos de saida
+     * @param instru ponteiro para saber quantas instruções vao ser lidas no arquivo geo
+     * @param formcriadas ponteiro para saber quantas serão as formas criadas no arquivo geo
+     * @param id ponteiro para pegar o id da ultima forma criada
+     * @param xcent ponteiro para pegar o valor x do baricentro do centro de gravidade
+     * @param ycent ponteiro para pegar o valor y do baricentro do centro de gravidade
+     * 
+     * @return void
+     */
+    ARVORE processa_geo(char* pathgeo, char* dirsaida, char* nomegeo, int *instru, int *formcriadas, int *id, float *xcent, float *ycent);
+#endif
